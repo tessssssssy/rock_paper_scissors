f73b9e4de9e24c4962514662d149f8ef8aca158a
tessssssssy <tessfabry@gmail.com>
2019-11-09 13:26:38 +1100

    main game logic

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 function computerPlay() {
   var randomNumber = Math.ceil(Math.random() * 3);
   if (randomNumber === 1) {
-    return "Rock";
+    return "rock";
   }
   if (randomNumber === 2) {
-    return "Paper";
+    return "paper";
   }
   if (randomNumber === 3) {
-    return "Scissors";
+    return "scissors";
   }
 }
-
-console.log(computerPlay());
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection > computerSelection) {
+  if (
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "paper") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")
+  ) {
     return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection < computerSelection) {
+  } else if (
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "rock") ||
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")
+  ) {
     return `You lose... ${computerSelection} beats ${playerSelection}`;
   } else {
-    return `Draw! You both selected ${playerSelection}!`;
+    return `Draw! You both selected ${computerSelection}!`;
   }
 }
 
-const playerSelection = 'rock'
-const computerSelection = computerPlay()
-
 function game() {
-    if 
+  const playerSelection = prompt("Rock, paper or scissors?");
+  const computerSelection = computerPlay();
+
+  //playRound(playerSelection, computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
 }
+
+game();
+
+//cases
+//player rock comp scissors - win
+//player paper comp rock - win
+//player scissors comp paper - win
+
+//player rock comp paper - lose
+//player scissors comp rock - lose
+//player paper comp scissors - lose
+
+//player === comp - draw
